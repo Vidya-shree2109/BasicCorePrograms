7f0c021ca4fe260c0154c8e575874b619d7c8705
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-11 19:56:50 +0530

    Percentage Of Heads and Tails Calculated

---
+++ BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class FlipCoin
+    {
+        public void Flip()
+        {
+            Random rand = new Random();
+            int heads = 0, tails = 0, result = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                result = rand.Next(0, 2);
+
+                if (result == 1)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+                double coin = rand.NextDouble();
+                if (coin < 0.5)
+                {
+                    Console.WriteLine(coin + " Tail");
+                    heads += 1;
+                }
+                else
+                {
+                    Console.WriteLine(coin + " Head");
+                    tails += 1;
+                }
+            }
+            Console.WriteLine("The Ratio of Tail:Head is : ({0}:{1})", heads, tails);
+            Console.WriteLine("Percentage of Tail is : {0}%", tails * 10);
+            Console.WriteLine("Percentage of Head is : {0}%", heads * 10);
+        }
+        
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("----------Welcome To Basic Programs----------");
+            bool finish = true;
+            Console.WriteLine("\n1.Flip Coin\n2.End the program");
+            while(finish)
+            {
+                Console.WriteLine("\nEnter an option to execute :");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1: FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.Flip();
+                        break;
+                    case 2: finish = false;
+                        break;
+
+                    default: Console.WriteLine("Enter a valid option !");
+                        break;
+                }
+            }
+
+        }
+    }
+}
